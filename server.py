f413632fd565a6e5cbe933948c6e7c10a6a95151
Maurya850 <mpr850@uregina.ca>
2025-05-05 10:30:47 -0600

    Adding the link the Certificate PDFs

--- server.py
+++ server.py
@@ -7,6 +7,11 @@ def mainPage():
 def edu():
     result_certi = db.session.execute(db.select(Certifcate)).scalars().all()
     return render_template('education.html',items = result_certi)
+@app.route('/work')
+def work():
+    result_work = db.session.execute(db.select(Work)).scalars().all()
+    return render_template('work.html',items = result_work)
+
 
 @app.route('/mobile')
 def mobilePage():
@@ -21,10 +26,8 @@ def frontEnd():
     result_frontend = db.session.execute(db.select(Frontend)).scalars().all()
 
     return render_template('details.html',items = result_frontend)
-@app.route('/workexp')
-def workExp():
-    return render_template('work.html')
+
 
 if __name__ == "__main__":
-    app.secret_key = 'SECRET KEY'
-    # app.run(debug=True)
+    # app.secret_key = 'SECRET KEY'
+    app.run(debug=True)
